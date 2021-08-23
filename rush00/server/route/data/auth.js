b2f07fb2c6708f47dad922e9a308ebe5d369cd11
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-08-24 01:26:46 +0900

    회원가입, 로그인 끝

--- rush00/server/route/data/auth.js
+++ rush00/server/route/data/auth.js
@@ -6,7 +6,7 @@ const { User } = require('../../models');
 const loginPost = async (req, res) => {
   const info = req.body;
   let cookie;
-
+  
   try {
     const userData = await User.findOne({
       where: {
@@ -30,16 +30,14 @@ const loginPost = async (req, res) => {
               },
               (err, token) => {
                 if (err) reject(err);
-                resolve(token);
+                else resolve(token);
               });
+            });
+          }
+          getToken().then(token => {
+            res.status(200).cookie('jwt_token', token, { httpOnly: true, maxAge: 60 * 60 * 1000 }).send('gogo');
           });
-        }
         
-        getToken().then(token => {
-          console.log(token);
-          res.cookie('jwt_token', token, { httpOnly: false, maxAge: 60 * 60 * 1000 });
-          res.json(cookie)
-        });
       }
     }
   } catch (err) {

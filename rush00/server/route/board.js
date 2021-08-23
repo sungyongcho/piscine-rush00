88d5aabc1f0e692f231b050852064370f3bcbbb5
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-08-24 02:25:26 +0900

    test

--- rush00/server/route/board.js
+++ rush00/server/route/board.js
@@ -2,7 +2,4 @@ const express = require('express');
 
 const router = express.Router();
 
-router
-  .post('/')
-
 module.exports = router;
--- rush00/server/server.js
+++ rush00/server/server.js
@@ -31,6 +31,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/', main);
 app.use('/account', account);
-app.use('/board', board);
+app.use('/board/content', board);
 
 app.listen(port, () => console.log(`Port ${port} is running ..,`));

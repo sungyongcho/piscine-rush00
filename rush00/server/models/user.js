099ac3af125feb2ca862d75033d3c16ade4e4f34
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2021-08-23 18:01:50 +0900

    Sequelize DB 연동

---
+++ rush00/server/models/user.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            username: {                         // username : 255자 아내, 필수, 고유한 값
+                type: Sequelize.STRING(255),
+                allowNull: false,
+                unique: true,
+            },
+            password: {                         // password : 255자 이내, 필수
+                type: Sequelize.STRING(255),
+                allowNull: false,
+            },
+            email: {                            // email : 255자 이내, 필수
+                type: Sequelize.STRING(255),
+                allowNull: false,
+            },
+        }, {
+            modelName: 'User',
+            tableName: 'users',
+            charset: 'utf8',
+            collate: 'utf8_general_ci'  // 한글 저장
+        })
+    }
+}
--- rush00/server/server.js
+++ rush00/server/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+
+const { sequelize } = require('./models');
 
 const app = express();
+
+const port = 80; // 나중에 환경변수 포트로 변경 필요
 
 // require('dotenv').config();
 
 // const port = process.env.EXPRESS_PORT || 3001;
-// app.set('port', port);
 
-const port = 80; // 나중에 환경변수 포트로 변경 필요
+app.set('port', port);
+
+sequelize.sync({ force: false })
+    .then(() => console.log('데이터베이스 연결 성공'))
+    .catch((err) => console.log(err))
 
 const main = require('./route/main');
 const account = require('./route/account');
 const board = require('./route/board');
 
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());

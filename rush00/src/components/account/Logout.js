aee2339720eccaae7b9b5dae5756a7d7091edc07
이지원 <ijiwon@ijiwon-ui-MacBookPro.local>
2021-08-24 17:14:01 +0900

    make Logout

---
+++ rush00/src/components/account/Logout.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const Logout = () => {
+  axios
+    .get('/account/logout')
+    .then((res) => {
+      window.location = res.data.redirect;
+    })
+    .catch(console.log);
+
+  window.location = '/';
+};
+
+export default Logout;
